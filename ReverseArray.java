ea59e0e4e6e3f227f6cef4af9d6924c6f1005503
Pranav <pranavpatil7743@gmail.com>
2024-01-05 19:03:32 +0530

    ramen

---
+++ ReverseArray.java
@@ -0,0 +1,25 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        reverse(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            // Swap elements at start and end
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            // Move towards the center
+            start++;
+            end--;
+        }
+    }
+}
